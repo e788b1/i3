637a69d1fd6fb918ba1f5dcd79270f42eac055da
e788b1 <e788b1@gmail.com>
2015-04-10 05:47:51 +0800

    update i3 config

--- update.sh
+++ update.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 cd ~/.i3
+files="
+    /etc/portage/make.conf
+    /etc/portage/package.use/e788b1.use
+    /var/lib/portage/world
+"
+for file in $files
+do
+    cp -v $file gentoo/
+done
+
 git add .
 git commit -m 'update i3 config'
 git push origin master
