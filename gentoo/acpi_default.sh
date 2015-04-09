9bcbd6717c875e1fcbf8f751ef1294edf6e63144
e788b1 <e788b1@gmail.com>
2015-04-10 06:29:32 +0800

    update

--- gentoo/acpi_default.sh
+++ gentoo/acpi_default.sh
@@ -5,26 +5,26 @@
 # $License: MIT (or 2-clause/new/simplified BSD)         Exp $
 # $Version: 2014/12/24 21:09:26                          Exp $
 #
- 
+
 log() { logger -p daemon "ACPI: $*"; }
 uhd() { log "event unhandled: $*"; }
- 
+
 set $*
 group=${1%/*}
 action=${1#*/}
 device=$2
 id=$3
 value=$4
- 
+
 [ -d /dev/snd ] && alsa=true || alsa=false
 [ -d /dev/oss ] && oss=true || oss=false
 amixer="amixer -q set Master"
 ossmix="ossmix -- vmix0-outvol"
- 
+
 case "$group" in
 	ac_adapter)
 		case "$value" in
-			*0) echo 0 >  /sys/class/backlight/acpi_video0/brightness;; 
+			*0) echo 0 >  /sys/class/backlight/acpi_video0/brightness;;
 			*1) echo 10 >  /sys/class/backlight/acpi_video0/brightness;;
 			*) uhd $*;;
 		esac
@@ -47,12 +47,12 @@ case "$group" in
 				;;
 			power) shutdown -H now;;
 			sleep) pm-suspend;;
-			mute) 
+			mute)
 				$alsa && $amixer toggle;;
-			volumeup) 
+			volumeup)
 				$alsa && $amixer 3dB+
 				$oss && $ossmix +3;;
-			volumedown) 
+			volumedown)
 				$alsa && $amixer 3dB-
 				$oss && $ossmix -3;;
 			*) uhd $*;;
@@ -60,10 +60,10 @@ case "$group" in
 		;;
 	cd)
 		case "$action" in
-			play) :;;
-			stop) :;;
-			prev) :;;
-			next) :;;
+			play) mpc toggle;;
+			stop) mpc stop;;
+			prev) mpc previous;;
+			next) mpc next;;
 			*) uhd $*;;
 		esac
 		;;
@@ -81,7 +81,7 @@ case "$group" in
 		;;
 	*) uhd $*;;
 esac
- 
+
 unset alsa oss amixer ossmix group action device id
- 
+
 # vim:fenc=utf-8:ft=sh:ci:pi:sts=4:sw=4:ts=4:
