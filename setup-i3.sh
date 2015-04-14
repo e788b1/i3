661ad3b78df55ae3f132b19661337a9346d49ba5
e788b1 <e788b1@gmail.com>
2015-04-14 21:30:20 +0800

    update

--- setup-i3.sh
+++ setup-i3.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 cd
-for file in `ls .i3/skel/*`
+rm -rv .thumbnails
+ln -sv /tmp/thumbnails .thumbnails
+
+for file in `ls -d ~/.i3/skel/*`
 do
-	ln -sfv $file '.'`basename $file`
+	if [ ! -d '.'`basename $file` ]; then
+		ln -sfv $file '.'`basename $file`
+	fi
 done
 
 mkdir .config/
-
-cp -v .i3/Trolltech.conf .config/Trolltech.conf
-
-rm -rv .thumbnails
-ln -sv /tmp/thumbnails .thumbnails
-[ ! -d .icons ] && ln -sv .i3/icons .icons
-[ ! -d .vim ] && ln -sv .i3/vim .vim
-ln -sfv .i3/vim/vimrc .vimrc
-
-[ ! -d .bash_completion.d ] && cp -rv .i3/bash_completion.d .bash_completion.d
-
-mkdir -v .config/fontconfig
-cp -v .i3/fonts.conf .config/fontconfig/fonts.conf
-# ln -v ${PWD}/.i3/fonts.conf .config/fontconfig/fonts.conf
-# [ ! -d .fonts ] && ln -sv .i3/fonts .fonts
-# cd .fonts
-# mkfontdir
-# mkfontscale
-# fc-cache -fv
-# cd
+cd .config/
+for file in `ls -d ~/.i3/config.d/*`
+do
+	if [ ! -d `basename $file` ]; then
+		ln -sfv $file `basename $file`
+	fi
+done
+cd
